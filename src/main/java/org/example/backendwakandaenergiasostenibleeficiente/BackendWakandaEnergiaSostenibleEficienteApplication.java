b5f6266e8c9988d75710e317bd94697665852494
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-11-26 14:07:08 +0100

    Initial commit

---
+++ src/main/java/org/example/backendwakandaenergiasostenibleeficiente/BackendWakandaEnergiaSostenibleEficienteApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaenergiasostenibleeficiente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWakandaEnergiaSostenibleEficienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWakandaEnergiaSostenibleEficienteApplication.class, args);
+    }
+
+}
